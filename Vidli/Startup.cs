030413e3a859df01e3f76e9dea48c9dcbc86c349
Mohammad Alam <mzabd162@gmail.com>
2018-01-08 11:55:33 -0500

    Add project files.

---
+++ Vidli/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidli.Startup))]
+namespace Vidli
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
