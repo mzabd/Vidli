b71f293ad5b87ceaa5e96478adbe6d6d63ffab88
Mohammad Alam <mzabd162@gmail.com>
2018-01-08 12:45:05 -0500

    Migrated all source code from Vidly to Vidli

---
+++ Vidli/Controllers/CustomersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidli.Models;
+using Vidli.ViewModels;
+
+namespace Vidli.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            //list of customers
+
+            var customers = GetCustomers();
+   
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer{Id = 1, Name = "John Smith"},
+                new Customer{Id = 2, Name = "Marry Williams"},
+
+            };
+
+            return customers;
+        }
+    }
+}
