cb595c968455fafe64c6bdbbdbb662744367cb44
Mohammad Alam <mzabd162@gmail.com>
2018-01-09 10:10:38 -0500

    Applied Annotations for CustomerName to make db schema override

--- Vidli/Models/Customer.cs
+++ Vidli/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace Vidli.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required] //for db schema: make it not nullable
+        [StringLength(255)] //to make nvarchar as 255
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
 
