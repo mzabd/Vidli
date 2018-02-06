90e15d838fc3130e96e9d9aff893c6cd8267dfc5
Mohammad Alam <mzabd162@gmail.com>
2018-02-06 12:24:45 -0500

    Added bootbx.js

--- Vidli/App_Start/BundleConfig.cs
+++ Vidli/App_Start/BundleConfig.cs
@@ -23,7 +23,8 @@ namespace Vidli
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                       "~/Scripts/bootbox.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css", "~/Content/themes/base/jquery-ui.min.css",
